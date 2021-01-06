d150a6b605d01ae333b521e8d7ca507fd8ea9f40
david espinal <despinal0425@gmail.com>
2021-01-05 23:11:33 -0500

    finished class lab

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Book from "./Components/Book"
 import Footer from "./Components/Footer"
 import Header from "./Components/Header"
+import faker from 'faker';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Book title="The Power of Now" description="Lorem dolor sit amet, an sea omnis luptaum, ea consul iudicabit duo. Cu enim lucilus vic, ut pri decore nostro patentium. Ne sea summo deleniti Ea omnes nominati pro. At vis propriae consequat torquantos, ex facer oratio mel. Quodsi tritani appareat his ei, atqui facilis quaestio enum eu." img="" />
+      <Book title="The Power of Now" description="Lorem dolor sit amet, an sea omnis luptaum, ea consul iudicabit duo. Cu enim lucilus vic, ut pri decore nostro patentium. Ne sea summo deleniti Ea omnes nominati pro. At vis propriae consequat torquantos, ex facer oratio mel. Quodsi tritani appareat his ei, atqui facilis quaestio enum eu." img={faker.image.image()} />
       <Book title="The Alchemist" description="Lorem dolor sit amet, an sea omnis luptaum, ea consul iudicabit duo. Cu enim lucilus vic, ut pri decore nostro patentium. Ne sea summo deleniti Ea omnes
       nominati pro. At vis propriae consequat torquantos, ex facer oratio mel. Quodsi tritani appareat his ei, atqui facilis
-      quaestio enum eu." img="" />
+      quaestio enum eu." img={faker.image.image()} />
       <Footer />
     </div>
   );
--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,11 +1,12 @@
 import './Book.css';
 
+
 function Book(prop) { 
   return (
     <div className="books"> 
       <h2>{prop.title}</h2>
       <div className="flex">
-        <img src='{img}' />
+        <img src={prop.img} />
         <p>{prop.description}</p>
       </div>
     </div>
